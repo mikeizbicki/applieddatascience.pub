b2d0f6e3ab5aa9c15729dfc6804de956c6dc0f6a
Mike Izbicki <mike@izbicki.me>
2019-12-09 08:43:04 -0800

    added google analytics

--- template.js
+++ template.js
@@ -1,3 +1,32 @@
+/*
+ * This code comes from Google Analytics and 
+ * https://stackoverflow.com/questions/51833090/put-google-analytics-code-in-an-js-file
+ */
+
+/*This function will load script and call the callback once the script has loaded*/
+function loadScriptAsync(scriptSrc, callback) {
+    if (typeof callback !== 'function') {
+        throw new Error('Not a valid callback for async script load');
+    }
+    var script = document.createElement('script');
+    script.onload = callback;
+    script.src = scriptSrc;
+    document.head.appendChild(script);
+}
+
+/* This is the part where you call the above defined function and "call back" your code which gets executed after the script has loaded */
+loadScriptAsync('https://www.googletagmanager.com/gtag/js?id=UA-36396447-3', function(){
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-36396447-3');
+})
+
+
+/*
+ * This code comes from distill.pub for formatting
+ */
+
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
